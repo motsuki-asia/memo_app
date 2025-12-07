a514d3a4e4e12cb4c60596add1785d4c8c41b91e
motsuki-asia <motsuki@asia-interg.com>
2025-12-07 15:23:01 +0900

    バリデーション機能の完成

--- resources/views/livewire/memos/create.blade.php
+++ resources/views/livewire/memos/create.blade.php
@@ -1,12 +1,19 @@
 <?php
 
-use function Livewire\Volt\{state};
+use function Livewire\Volt\{state, rules};
 use App\Models\Memo;
 
 state(['title', 'body']);
 
+// バリデーションルールを定義
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
 // メモを保存する関数
 $store = function () {
+    $this->validate(); // バリデーションチェック
     // フォームからの入力値をデータベースへ保存
     Memo::create($this->all());
     // 一覧ページにリダイレクト
@@ -21,11 +28,20 @@ $store = function () {
 
     <form wire:submit="store">
         <p>
-            <laravel for="title">タイトル</><br>
+            <laravel for="title">タイトル</>
+                @error('title')
+                    <span class="error">({{ $message }})</span>
+                @enderror
+                <br>
                 <input type="text" wire:model="title" id="title">
                 <p>
                 <p>
-                    <laravel for="body">本文</><br>
+                    <laravel for="body">本文</>
+                        <label for="body">本文</label>
+                        @error('body')
+                            <span class="error">({{ $message }})</span>
+                        @enderror
+                        <br>
                         <textarea wire:model="body" id="body"></textarea>
                         <p>
 
---
+++ resources/views/livewire/memos/edit.blade.php
@@ -0,0 +1,55 @@
+<?php
+
+use function Livewire\Volt\{state, mount, rules};
+use App\Models\Memo;
+
+// フォームの状態を管理
+state(['memo', 'title', 'body']);
+
+// ルートモデルバインディングはmountでまとめて行う
+mount(function (Memo $memo) {
+    $this->memo = $memo;
+    $this->title = $memo->title;
+    $this->body = $memo->body;
+});
+
+// バリデーションルールを定義
+rules([
+    'title' => 'required|string|max:50',
+    'body' => 'required|string|max:2000',
+]);
+
+// メモを更新する関数
+$update = function () {
+    $this->validate(); // バリデーションチェック
+    $this->memo->update($this->all());
+    return redirect()->route('memos.show', $this->memo);
+};
+?>
+
+<div>
+    <a href="{{ route('memos.show', $memo) }}">戻る</a>
+    <h1>更新</h1>
+
+    <form wire:submit="update">
+        <p>
+            <laravel for="title">タイトル</>
+                @error('title')
+                    <span class="error">({{ $message }})</span>
+                @enderror
+                <br>
+                <input type="text" wire:model="title" id="title">
+        </p>
+        <p>
+            <laravel for="body">本文</>
+                <label for="body">本文</label>
+                @error('body')
+                    <span class="error">({{ $message }})</span>
+                @enderror
+                <br>
+                <input type="text" wire:model="body" id="body">
+        </p>
+
+        <button type="submit">更新</button>
+    </form>
+</div>
